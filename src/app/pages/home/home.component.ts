b3599d8998420e3ef9a4982a3b45481ef874578c
chainsensedhiraj <dhiraj.patil@chainsense.in>
2023-03-08 20:24:40 +0530

    send call for single address

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ApiService } from 'src/app/services/api.service';
+import { Web3Service } from 'src/app/services/web3.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-home',
@@ -7,18 +8,31 @@ import { ApiService } from 'src/app/services/api.service';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  qty:any=0;
   constructor(
-    public apiService:ApiService
+    public web3 :Web3Service
   ) { }
 
+ 
   ngOnInit(): void {
-    this.apiService.getExchangesList().then(res=>{
-      console.log(res);
-      this.apiService.exchanges = res.data;
-    }).catch(err=>{
-      console.log(err);
-    });
+    if(localStorage.getItem('addresses')){
+      let temp = localStorage.getItem('addresses') || '';
+      this.web3.addresses = JSON.parse(temp);
+    }
+  }
+  connectWallet(){
+    this.web3.connectWallet();
   }
 
+  async geneRate(){
+    if(this.qty)await this.web3.generateAccounts(this.qty);
+
+  }
+
+  async transfer(){
+    let contractAddr = environment.ecr20Token;
+    
+    await this.web3.contractPayable('transfer', [this.web3.addresses[0].address,'1000000000000000000'],0, 'erc20Abi', contractAddr)
+
+  }
 }
